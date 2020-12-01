9b7af12854107f9366636d5b9c040a1823a550d1
LiliClift <65939150+LiliCecilia23@users.noreply.github.com>
2020-12-01 16:34:49 -0600

    changing functional component to class component on table

--- employee-directory/src/components/Table.js
+++ employee-directory/src/components/Table.js
@@ -1,38 +1,41 @@
-import React from "react";
+import React, { Component } from "react";
 import "../App.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-function Table(props){
-    return(
-        <div className="rounded mx-5">
-            <table className="table table-striped table-dark rounded">
-                <thead>
-                    <tr>
-                    <th scope="col"><button className="btn btn-info">ID</button></th>
-                    <th scope="col"><button className="btn btn-info">FIRST</button></th>
-                    <th scope="col"><button className="btn btn-info">LAST</button></th>
-                    <th scope="col"><button className="btn btn-info">ROLE</button></th>
-                    <th scope="col"><button className="btn btn-info">SALARY</button></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    
-                    {props.employees.map(employee => (
-                        
+class Table extends Component {
+    
+    render(props){
+        return(
+            <div className="rounded mx-5">
+                <table className="table table-striped table-dark rounded">
+                    <thead>
                         <tr>
-                            <th scope="row" key={employee.id}>{employee.id}</th>
-                            <td>{employee.firstName}</td>
-                            <td>{employee.lastName}</td>
-                            <td>{employee.role}</td>
-                            <td>{employee.salary}</td>
-                         </tr>
+                        <th scope="col"><button className="btn btn-info">ID</button></th>
+                        <th scope="col"><button className="btn btn-info">FIRST</button></th>
+                        <th scope="col"><button className="btn btn-info">LAST</button></th>
+                        <th scope="col"><button className="btn btn-info">ROLE</button></th>
+                        <th scope="col"><button className="btn btn-info">SALARY</button></th>
+                        </tr>
+                    </thead>
+                    <tbody>
                     
-                    ))}
+                        {props.employees.map(employee => (
+                            
+                            <tr>
+                                <th scope="row" key={employee.id}>{employee.id}</th>
+                                <td>{employee.firstName}</td>
+                                <td>{employee.lastName}</td>
+                                <td>{employee.role}</td>
+                                <td>{employee.salary}</td>
+                            </tr>
                     
-                </tbody>
-            </table>
-        </div>
-    )
+                        ))}
+                    
+                    </tbody>
+                </table>
+            </div>
+        )
+    }
 }
 
 export default Table;
