fd01b3b8e33711fd85c67093c355603b1e397f7f
LiliClift <65939150+LiliCecilia23@users.noreply.github.com>
2020-12-03 13:30:22 -0600

    photos working

--- employee-directory/src/components/Table.js
+++ employee-directory/src/components/Table.js
@@ -32,30 +32,29 @@ class Table extends Component {
     }
     
     render(){
+        
         return(
             <div className="rounded mx-5">
                 <table className="table table-striped table-dark rounded">
                     <thead>
                         <tr>
-                        <th scope="col"><button className="btn btn-info" data-value="id">ID</button></th>
+                        <th scope="col">PHOTO</th>
                         <th scope="col"><button className="btn btn-info" data-value="first">FIRST</button></th>
                         <th scope="col"><button className="btn btn-info" data-value="last">LAST</button></th>
                         <th scope="col"><button className="btn btn-info" data-value="email">EMAIL</button></th>
                         <th scope="col"><button className="btn btn-info" data-value="cell">CELL</button></th>
-                        <th scope="col"><button className="btn btn-info" data-value="picture">PICTURE</button></th>
                         </tr>
                     </thead>
                     <tbody>
-                        {console.log(this.state.employees)}
+    
                         {this.state.employees.map(employee => (
                             
                             <tr>
-                                <th scope="row" key={employee.id.value}>{employee.id.value}</th>
+                                <th scope="row"><img src={employee.picture.thumbnail} alt="employee" className="rounded"></img></th>
                                 <td>{employee.name.first}</td>
                                 <td>{employee.name.last}</td>
                                 <td>{employee.email}</td>
                                 <td>{employee.cell}</td>
-                                <td>{employee.picture.thumbnail}</td>
                             </tr>
                     
                         ))}
